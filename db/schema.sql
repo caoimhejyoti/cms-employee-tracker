c4a892215525b65e2854584e48e8d73f185b8676
caoimhejyoti <caoimhejyoti@gmail.com>
2023-02-15 12:03:51 +0800

    updating table values and seeds to reflect

--- db/schema.sql
+++ db/schema.sql
@@ -16,7 +16,7 @@ CREATE TABLE department (
 CREATE TABLE role (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) NOT NULL UNIQUE,
-  salary DECIMAL (10,2) NOT NULL,
+  salary DECIMAL NOT NULL,
   department_id INT,
   FOREIGN KEY (department_id) REFERENCES department(id)
 );
@@ -28,7 +28,7 @@ CREATE TABLE employee (
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT,
-    manager_name VARCHAR(60),
+    manager_id INT,
     FOREIGN KEY (role_id) REFERENCES role(id)
 );
 
--- db/seeds.sql
+++ db/seeds.sql
@@ -7,24 +7,24 @@ VALUES  ('HR'),
 
 
 INSERT INTO role (title, salary, department_id)
-VALUES  ('Sales Team Manager', 100000.00, 2),
-        ('Salesperson', 80000.00, 2),
-        ('Leagal Team Manager', 250000.00, 3),
-        ('Lawyer', 190000.00, 3),
-        ('Engineering Team Manager', 150000.00, 4),
-        ('Software Engineer', 120000.00, 4),
-        ('Finance Team Manager', 160000.00, 5),
-        ('Accountant', 120000.00, 5);
+VALUES  ('Sales Team Manager', 100000, 2),
+        ('Salesperson', 80000, 2),
+        ('Leagal Team Manager', 250000, 3),
+        ('Lawyer', 190000, 3),
+        ('Engineering Team Manager', 150000, 4),
+        ('Software Engineer', 120000, 4),
+        ('Finance Team Manager', 160000, 5),
+        ('Accountant', 120000, 5);
 
-INSERT INTO employee (first_name, last_name, role_id, manager_name)
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES  ('Rory', 'Gilmore', 1, NULL),
-        ('Lane', 'Kim', 2, 'Rory Gilmore'),
+        ('Lane', 'Kim', 2, 1),
         ('Lorelai', 'Gilmore', 3, NULL ),
-        ('Luke', 'Danes', 4, 'Lorelai Gilmore'),
+        ('Luke', 'Danes', 4, 3),
         ('Sookie', 'St.James', 5, NULL),
-        ('Kirk', 'Gleason', 6, 'Sookie St.James'),
+        ('Kirk', 'Gleason', 6, 5),
         ('Richard', 'Gilmore', 7, NULL),
-        ('Emily', 'Gilmore', 8, "Richard Gilmore");
+        ('Emily', 'Gilmore', 8, 7);
 
 
 -- INSERT INTO manager (first_name, last_name, role_id)
